a1b7482c3810ddf8627b9a8c18bcf188dd937ecf
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-01-10 14:27:52 +0100

    end bonus

--- js/scripts.js
+++ js/scripts.js
@@ -12,16 +12,20 @@ createApp({
     methods:{
 
     },
-    mounted(){
+    created(){
         //ciclo per creare 10 email casuali tramite chiamata di api random mail
-        for (let i = 0; i < 10; i++) {
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then((objEmail) =>{
-                this.email.push(objEmail.data.response);    
-                console.log(objEmail.data.response, typeof objEmail.data.response)
-            });
-        }
-
+        generatore(this.email);
+    },
+    mounted(){
     }
   // Monto l'istanza di Vue in pagina
 }).mount('#app');
+
+function generatore(array){
+    for (let i = 0; i < 10; i++) {
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((objEmail) =>{
+            array.push(objEmail.data.response);    
+        });
+    }
+}
