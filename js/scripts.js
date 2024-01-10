194e24cccc21853e202c11d1135a6f353af8a03d
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-01-10 13:31:02 +0100

    project start

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
 // Estraggo la funzione createApp dall'oggetto Vue
 const { createApp } = Vue;
 
-// Creo l'istanza di Vue da mondare in pagina
+// Creo l'istanza di Vue da montare in pagina
 createApp({
     data() {
         return {
             message: 'Template HTML CSS Vue',
         };
+    },
+    methods:{
+
+    },
+    mounted(){
+
     }
   // Monto l'istanza di Vue in pagina
 }).mount('#app');
